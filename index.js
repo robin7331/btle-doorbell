b9c7b97d6b8dafb3bd861ce405ddcf499a68b7ed
Robin Reiter <robin7331@gmail.com>
2016-09-01 15:13:09 +0200

    cleanup

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 
    let noble = require('noble');
    let mqtt = require('mqtt');
+
+   // Connect to the local instance of Mosquitto
    let client = mqtt.connect('mqtt://localhost');
 
+   // Simply log the successful connect.
    client.on('connect', function() {
       console.log("connected to mqtt service");
    })
 
+   // Once BT is powered on start scanning for devices
    noble.on('stateChange', function(state) {
       if (state === 'poweredOn') {
          noble.startScanning([], true, function(error) {
@@ -17,14 +21,18 @@
       }
    });
 
-   noble.on('RFduino', function(peripheral) {
+
+   // When a device was found check if it is our doorbell sender.
+   noble.on('discover', function(peripheral) {
       console.log("Found a device called '" + peripheral.advertisement.localName + "'");
 
       let name = peripheral.advertisement.localName;
 
+      // If yes stop scanning and connect to the device.
       if (name == "doorbell") {
          noble.stopScanning();
 
+         // once the device turns off start scanning again.
          peripheral.on('disconnect', function(){
             noble.startScanning([], true, function(error) {
                console.log("Scanning error " + JSON.stringify(error, null, 4));
@@ -51,8 +59,9 @@
                               characteristic.subscribe();
                               characteristic.on('data', function(data, isNotification) {
                                  console.log("Open the frikin door!");
-                                 client.publish('office/door/events', 'rings');
 
+                                 // publish an MQTT message though mosquitto.
+                                 client.publish('office/door/events', 'rings');
                               });
                            }
                         }
